6b76be550eccc5c2de2efb4eb61c8aecc9a87f69
utkarshadahibhate <utkarsha.dahibhate@gmail.com>
2026-03-25 19:10:16 +0000

    26/03/26_v1

--- Doubly_Linked_List/DLL_allcombine.cpp
+++ Doubly_Linked_List/DLL_allcombine.cpp
@@ -98,6 +98,7 @@ Node* Node :: insert_at_pos(Node *head, int data, int pos){
         if(head != NULL){
             head->prev = new_node;
         }
+        head = new_node;
         return head;
     }
 
@@ -152,7 +153,7 @@ Node* Node :: delete_at_start(Node *head){
 Node* Node :: delete_at_end(Node *head){
     Node *lastnode = head;
 
-    if(head = NULL){
+    if(head == NULL){
         return head;
     }
 
@@ -161,8 +162,10 @@ Node* Node :: delete_at_end(Node *head){
         return NULL;
     }
     //traverse till last node
-    while(lastnode->next != NULL){
-        lastnode = lastnode->next;
+    if(head != NULL){
+        while(lastnode->next != NULL){
+            lastnode = lastnode->next;
+        }
     }
     //update second last node pointers
     lastnode->prev->next = NULL;
@@ -189,7 +192,8 @@ Node* Node :: delete_at_pos(Node *head, int pos){
     return head;
 
     if(pos > count_nodes(head)){
-        cout<<"Invalid position, nodes out of bounds !"<<endl;
+        cout<<"\nInvalid position, nodes out of bounds !"<<endl;
+        cout<<"try again !"<<endl;
         return head;
     }
     else{
@@ -231,9 +235,9 @@ int Node :: count_nodes(Node *head){
 //to display the list
 void Node :: display(Node *head){
     Node *temp = head;
-    cout<<"DLL elements are : "<<endl;
+    cout<<"\nDLL elements are : "<<endl;
     if(temp == NULL){
-        cout<<"List is empty! "<<endl;
+        cout<<"\nList is empty! "<<endl;
     }
     while(temp != NULL){
         cout<<temp->data;
@@ -245,7 +249,112 @@ void Node :: display(Node *head){
 }
 
 int main(){
-    cout<<"Select a option :"<<endl;
-    cout<<"1)Creating a DLL \n2)Inserting an element \
-    \n3)Deleting an element"<<endl;
+    Node *head = NULL;
+    int choice;
+    // cin>>choice;
+    // cout<<"Select a option :"<<endl;
+    // cout<<"1)Creating a DLL \n2)Inserting an element \
+    // \n3)Deleting an element"<<endl;
+
+    while(true){
+        cout<<"\nSelect a option :"<<endl;
+        cout<<"1)Creating a DLL \n2)Inserting an element \
+        \n3)Deleting an element \n4)Display DLL \n5)Exit....."<<endl;
+        cin>>choice;
+
+        //switch case for above cases
+        switch(choice){
+            //1)Creating a DLL
+            case 1:
+            int nodes_num, ele, insert_choice;
+            cout<<"\nGive the number of nodes to be added : ";
+            cin>>nodes_num;
+            cout<<"\nSelect one \n1)Forward Insertion\n2)Backward Insertion"<<endl;
+            cin>>insert_choice;
+            for(int i = 1; i <= nodes_num; i++){
+                cout<<"give the element no "<<i<<": ";
+                cin>>ele;
+                if(insert_choice == 1){
+                    head = head->insert_at_front(head, ele);
+                }
+                else if(insert_choice == 2){
+                    head = head->insert_at_end(head, ele);
+                }
+                else{
+                    cout<<"Give a valid choice !"<<endl;
+                }
+            }
+            head->display(head);
+            break;
+
+            //2)Inserting an element
+            case 2:
+            //int insert_choice, ele, pos;
+            int pos;
+            cout<<"\nSelect one \n1)Insert at front \n2)Insert at the end \
+            \n3)Insert at specific position"<<endl;
+            cin>>insert_choice;
+            cout<<"Give an element to be added : ";
+            cin>>ele;
+            //cases
+            if(insert_choice == 1){
+                head = head->insert_at_front(head,ele);
+            }
+            else if(insert_choice == 2){
+                head = head->insert_at_end(head,ele);
+            }
+            else if(insert_choice == 3){
+                cout<<"Give a position number : ";
+                cin>>pos;
+                head = head->insert_at_pos(head,ele,pos);
+            }
+            else{
+                cout<<"Give a valid choice !"<<endl;
+            }
+            head->display(head);
+            break;
+
+            //3)Deleting an element
+            case 3:
+            cout<<"\nSelect one \n1)Delete at beginning \n2)Delete at the end \
+            \n3)Delete at specific position"<<endl;
+            cin>>insert_choice;
+            //cout<<"Give the element to be deleted : ";
+            //cin>>ele;
+            //cases
+            if(insert_choice == 1){
+                head = head->delete_at_start(head);
+            }
+            else if(insert_choice == 2){
+                head = head->delete_at_end(head);
+            }
+            else if(insert_choice == 3){
+                cout<<"Give a position number : ";
+                cin>>pos;
+                head = head->delete_at_pos(head,pos);
+            }
+            else{
+                cout<<"Give a valid choice !"<<endl;
+            }
+            head->display(head);
+            break;
+
+            //to display
+            case 4:
+            head->display(head);
+            break;
+
+            //exit case
+            case 5:
+            goto exit_loop;
+
+            //default case
+            default:
+            cout<<"\nInvalid input, please try again !"<<endl;
+            break;
+        }
+        cout<<endl;
+    }
+    exit_loop:
+    cout<<"Exited the loop."<<endl;
 }
